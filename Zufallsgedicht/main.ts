936fc6736270cba61e654dbfd1c3e4d201c28d6d
TheKey <schneider.niels@web.de>
2020-05-05 20:47:37 +0200

    Create main.ts

---
+++ Zufallsgedicht/main.ts
@@ -0,0 +1,25 @@
+let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren"];
+let wordDictionary: any = [subjects, verbs, objects];
+
+for (let i: number = 0; i < 5; i++) {
+    console.log(getVerse());
+}
+
+
+function getVerse(): string {
+    let sentence: string = "";
+    wordDictionary.forEach(function (item: string[]) {
+        sentence += chooseStringFromArray(item);
+    });
+    return sentence;
+}
+
+function chooseStringFromArray(string_array: string[]): string {
+    return string_array.splice(getRandomNumber(subjects.length), 1)[0] + " ";
+}
+
+function getRandomNumber(max: number): number {
+    return Math.floor(Math.random() * Math.floor(max));
+}
